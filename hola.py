a3fd27a9cafd6474e662005377e6fee367386a2c
Arehandoro <heiderich@Heiderich-PC.(none)>
2013-04-29 02:36:20 -0400

    Problema_01

---
+++ hola.py
@@ -0,0 +1,10 @@
+#UNIVERIDAD DE SANTIAGO DE CHILE
+#Carrera:  Licenciatura en Ciencias de la Computación.
+#Asignatura:  Lenguaje y Técnicas de Programación.
+#Profesor:  Igor Caracci.
+#Ayudante:  Andrés Caro Q.
+#Autor del Programa:  Alejandro Alberto Sánchez Iturriaga.
+#Problema 01 - Github:
+
+nombre = input("Ingrese su Nombre:  ")
+print("Hola,",nombre,"xD")
